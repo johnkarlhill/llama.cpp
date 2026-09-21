754ff1bb17a80ac5a7b77ccb43457b148cef8573
Ozymandias_EBON <johnhill@johnkarlhill.com>
2026-09-21 00:16:43 -0500

    perf: route tg base fn through v6 (2 rows/warp)

--- ggml/src/ggml-sycl/mmvq.cpp
+++ ggml/src/ggml-sycl/mmvq.cpp
@@ -1797,7 +1797,7 @@ static void mul_mat_vec_pq2_0_q8_1_v3_sycl(const void * vx, const void * vy,
         cgh.parallel_for(
             sycl::nd_range<3>(block_nums * block_dims, block_dims),
             [=](sycl::nd_item<3> item_ct1) [[sycl::reqd_sub_group_size(WARP_SIZE)]] {
-                mul_mat_vec_pq2_0_v5<QK_PQ2_0, QI_PQ2_0, block_pq2_0, VDR_PQ2_0_Q8_1_MMVQ, vec_dot_pq2_0_q8_1_swar, 1>(vx, vy, dst, ncols, nrows, 0, 0, item_ct1);
+                mul_mat_vec_pq2_0_v6<QK_PQ2_0, QI_PQ2_0, block_pq2_0, VDR_PQ2_0_Q8_1_MMVQ, vec_dot_pq2_0_q8_1_swar, 1>(vx, vy, dst, ncols, nrows, 0, 0, item_ct1);
             });
     });
 }
